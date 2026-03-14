bcd86b57ce0203241fa5d2805ec6f8e6606a3a2d
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2026-03-15 00:32:22 +0100

    feat: Create LoxInstance

--- src/main/java/com/zekumoru/lox/LoxClass.java
+++ src/main/java/com/zekumoru/lox/LoxClass.java
@@ -3,11 +3,22 @@ package com.zekumoru.lox;
 import java.util.List;
 import java.util.Map;
 
-class LoxClass {
+class LoxClass implements LoxCallable {
     final String name;
 
     LoxClass(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        LoxInstance instance = new LoxInstance(this);
+        return instance;
     }
 
     @Override
